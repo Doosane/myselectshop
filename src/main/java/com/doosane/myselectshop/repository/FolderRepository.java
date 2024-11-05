513bf2e59f7fc47effc2692c4d95fe414c0f0951
baekdoosan <doosan0425@gmail.com>
2024-11-05 13:29:27 +0900

    상품 폴더 설계

---
+++ src/main/java/com/doosane/myselectshop/repository/FolderRepository.java
@@ -0,0 +1,8 @@
+package com.doosane.myselectshop.repository;
+
+import com.doosane.myselectshop.entity.Folder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface FolderRepository extends JpaRepository<Folder, Integer> {
+
+}
---
+++ src/main/java/com/doosane/myselectshop/repository/ProductFolderRepository.java
@@ -0,0 +1,7 @@
+package com.doosane.myselectshop.repository;
+
+import com.doosane.myselectshop.entity.ProductFolder;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductFolderRepository extends JpaRepository<ProductFolder, Long> {
+}
