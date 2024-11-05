a72076978ec424a8b0821f13a94cdf05164305d1
baekdoosan <doosan0425@gmail.com>
2024-11-05 19:04:56 +0900

    AWS RDS 설정 추가

--- src/test/java/com/doosane/myselectshop/MyselectshopApplicationTests.java
+++ src/test/java/com/doosane/myselectshop/MyselectshopApplicationTests.java
@@ -1,13 +1,13 @@
-package com.doosane.myselectshop;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class MyselectshopApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
-}
+//package com.doosane.myselectshop;
+//
+//import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+//
+//@SpringBootTest
+//class MyselectshopApplicationTests {
+//
+//    @Test
+//    void contextLoads() {
+//    }
+//
+//}
