a08b743fc3f793d7eea5cfdd7748c94904e4101b
baekdoosan <doosan0425@gmail.com>
2024-11-07 13:06:11 +0900

    error 메시지 관리하기

--- src/main/java/com/doosane/myselectshop/service/ProductService.java
+++ src/main/java/com/doosane/myselectshop/service/ProductService.java
@@ -4,11 +4,13 @@ import com.doosane.myselectshop.dto.ProductMypriceRequestDto;
 import com.doosane.myselectshop.dto.ProductRequestDto;
 import com.doosane.myselectshop.dto.ProductResponseDto;
 import com.doosane.myselectshop.entity.*;
+import com.doosane.myselectshop.exception.ProductNotFoundException;
 import com.doosane.myselectshop.naver.dto.ItemDto;
 import com.doosane.myselectshop.repository.FolderRepository;
 import com.doosane.myselectshop.repository.ProductFolderRepository;
 import com.doosane.myselectshop.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
@@ -18,6 +20,7 @@ import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.Optional;
 
 @Service
@@ -27,6 +30,7 @@ public class ProductService {
     private final ProductRepository productRepository;
     private final FolderRepository folderRepository;
     private final ProductFolderRepository productFolderRepository;
+    private final MessageSource messageSource; // 스프링부트에서 MessageSource를 자동으로 구현 해 놓은것을 가져다가 주입받아서 쓰면된다.
 
     public static final int MIN_MY_PRICE = 100; // 최솟값 설정
 
@@ -40,11 +44,25 @@ public class ProductService {
     public ProductResponseDto updateProduct(Long id, ProductMypriceRequestDto requestDto) {
         int myprice = requestDto.getMyprice();
         if (myprice < MIN_MY_PRICE) {
-            throw new IllegalArgumentException("유효하지 않은 관심 가격입니다. 최소 " + MIN_MY_PRICE + "원 이상으로 설정해 주세요.");
+            throw new IllegalArgumentException(
+                    messageSource.getMessage(
+                            "below.min.my.price",
+                                     new Integer[]{MIN_MY_PRICE},
+
+                            "Wrong Price",
+                                         Locale.getDefault()
+
+                    )
+            );
         }
 
         Product product = productRepository.findById(id).orElseThrow(() ->
-                new NullPointerException("해당 상품을 찾을 수 없습니다.")
+                new ProductNotFoundException(messageSource.getMessage(
+                        "not.found.product",
+                        null,
+                        "Not Found Product",
+                        Locale.getDefault()
+                ))
         );
 
         product.update(requestDto);
